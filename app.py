bbae344b471a27c56372bab7d850d0292d378507
Gilson Zurc <51736921+GilsonZurc@users.noreply.github.com>
2026-02-15 16:53:34 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("App de Estudos para Concursos 2026 - Gilson")
+st.write("Bem-vindo! Teste de deploy na Vercel.")
+
+if st.button("Clique aqui"):
+    st.balloons()
